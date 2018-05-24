7b5e4e5862114e1e5e125c50b45c504b5b42de52
A-Rotsch <39245447+A-Rotsch@users.noreply.github.com>
2018-05-24 18:15:09 -0500

    Update Flyer.java

--- Flyer.java
+++ Flyer.java
@@ -31,6 +31,7 @@ public class Flyer {
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Press 1 for pickup, press 2 for delivery");
 		answer = scan.nextInt();
+		
 	if(answer == 1)
 	{
 		dc = 0;
@@ -40,17 +41,25 @@ public class Flyer {
 		{
 		System.out.print("Please enter your zip code.");
 		zip = scan.nextInt();
+		}
 		if(zip>=zip1 && zip<= zip2)
+		{
 			dc = 5;
 			if(dc == 5)
 			System.out.print("Delivery is available at a cost of $5");
+		}
 		if(zip == ezip1 || zip == ezip2)
+		{
 			dc = 7;
 			if(dc == 7)
 			System.out.print("Delivery with extra cost at $7.");
+		}
 		else
+		{
 			nozip = 1;
+		}
 		if(nozip == 1)
+		{
 			System.out.print("Delivery unavailable you will need to pick-up your order.");			
 		}
 	System.out.println("\nWould you like to order a Flyers' Burger for $4.50?\nPress 1 for yes, 2 for no");
