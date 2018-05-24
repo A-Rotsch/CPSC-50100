de56417f37fef724a819e4c8aab3ecb30acb0de3
A-Rotsch <39245447+A-Rotsch@users.noreply.github.com>
2018-05-24 17:58:04 -0500

    Update Flyer.java

--- Flyer.java
+++ Flyer.java
@@ -15,6 +15,19 @@ public class Flyer {
 		int nozip = 0;
 		int bq = 0; // Burger question
 		int ba =0; //  Burger quantity
+		int dq = 0;
+		int da = 0;
+		int fq = 0;
+		int fa = 0;
+		int fdq = 0;
+		int fda = 0;
+		double burger = 4.50;
+		double drink = 1.50;
+		double fries = 2.50;
+		double dessert = 3.00;
+		double cost = 0;
+		final double TAX = 1.05;
+		double total = 0;
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Press 1 for pickup, press 2 for delivery");
 		answer = scan.nextInt();
@@ -46,8 +59,35 @@ public class Flyer {
 		System.out.println("How many would you like?");
 	if(bq == 1)
 		ba = scan.nextInt();
-	System.out.print(ba);
+		double bc = (ba*burger);
 	
+	System.out.println("\nWould you like to order a Flyers' Drink for $1.50?\nPress 1 for yes, 2 for no");
+	dq = scan.nextInt();
+	if(dq == 1)
+		System.out.println("How many would you like?");
+	if(dq == 1)
+		da = scan.nextInt();
+		double dcc = (da*drink);
+	
+	System.out.println("\nWould you like to order a Flyers' Fries for $2.50?\nPress 1 for yes, 2 for no");
+	fq = scan.nextInt();
+	if(fq == 1)
+		System.out.println("How many would you like?");
+	if(fq == 1)
+		fa = scan.nextInt();
+		double fc = (fa*fries);
+		
+	System.out.println("\nWould you like to order a Flyers' Dessert for $3.00?\nPress 1 for yes, 2 for no");
+	fdq = scan.nextInt();
+	if(fdq == 1)
+		System.out.println("How many would you like?");
+	if(fdq == 1)
+		fda = scan.nextInt();
+		double fdc = (fda*dessert);
+	
+	cost = (bc+dcc+fc+fdc);
+	total = ((cost*TAX)+dc);
+	System.out.print("Your order including tax will cost: "+total);
 	}
 
 }
