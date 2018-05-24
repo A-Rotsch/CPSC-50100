cc04dc26c7d8a560971ab551767adbdd0d00ad99
A-Rotsch <39245447+A-Rotsch@users.noreply.github.com>
2018-05-24 17:36:59 -0500

    Update Flyer.java

--- Flyer.java
+++ Flyer.java
@@ -5,41 +5,49 @@ public class Flyer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int dc; //Delivery cost
-		final double TAX = .05; //tax to be charged
-		String answer; //Pick-up or Delivery object
-
+		int answer = 0;
+		int dc = 0;
+		int zip = 0;
+		int zip1 = 60442;
+		int zip2 = 60550;
+		int ezip1 = 60441;
+		int ezip2 = 60551;
+		int nozip = 0;
+		int bq = 0; // Burger question
+		int ba =0; //  Burger quantity
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Pick-up or Delivery");
-		answer = scan.nextLine();
-		if(answer == "Pick-up")
+		System.out.print("Press 1 for pickup, press 2 for delivery");
+		answer = scan.nextInt();
+	if(answer == 1)
+	{
+		dc = 0;
+	}
+	
+	if(answer == 2)
 		{
-			dc = 0;
-		}
-		if(answer == "Delivery")
-		{
-			System.out.print("Please enter your zip code.");
-			
-			double zip = scan.nextInt();
-			double zip1 = 60412;
-			double zip2 = 60550;
-			double ezip1 = 60441;
-			double ezip2 = 60551;
-			
-		if(zip>= zip1 && zip <= zip2)
-				dc = 5;
-				System.out.print("Delivery is available at a cost of $5.");
-		if (zip == ezip1 || zip == ezip2)
-				dc = 7;
-				System.out.print("Delivery with extra cost at $7.");
-		//else
-		//		System.out.print("Delivery is not available");
-		}
+		System.out.print("Please enter your zip code.");
+		zip = scan.nextInt();
+		if(zip>=zip1 && zip<= zip2)
+			dc = 5;
+			if(dc == 5)
+			System.out.print("Delivery is available at a cost of $5");
+		if(zip == ezip1 || zip == ezip2)
+			dc = 7;
+			if(dc == 7)
+			System.out.print("Delivery with extra cost at $7.");
 		else
-			System.out.print("Please try again.");
-
-			
-			
+			nozip = 1;
+		if(nozip == 1)
+			System.out.print("Delivery unavailable you will need to pick-up your order.");			
+		}
+	System.out.println("\nWould you like to order a Flyers' Burger for $4.50?\nPress 1 for yes, 2 for no");
+	bq = scan.nextInt();
+	if(bq == 1)
+		System.out.println("How many would you like?");
+	if(bq == 1)
+		ba = scan.nextInt();
+	System.out.print(ba);
+	
 	}
 
 }
